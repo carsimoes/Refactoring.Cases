0109eb7792fca331a6459718d75b1f665e0c1c07
carsimoes <carlos.renato.simoes@gmail.com>
2023-02-02 17:27:47 -0300

    Original algorithm to be refactored

--- Refactoring.Cases/Case1/Statement.cs
+++ Refactoring.Cases/Case1/Statement.cs
@@ -13,30 +13,86 @@ namespace Refactoring.Cases.Case1
     {
         public List<Invoice> Invoices;
         public List<Play> Plays;
+        public string Result;
 
         public Statement()
         {
+            CreateData();
+
+            var totalAmount = 0;
+            var volumeCredits = 0;
+            var result = $"Statment for {Invoices[0].Custumer}";
+            var format = 0.0;
+
+            foreach (var perf in Invoices[0].Performances)
+            {
+                var play = Plays.Where(x=>x.PlayID == perf.PlayID).FirstOrDefault(); 
+                var thisAmount = 0;
+
+                switch (play.PlayDetails.Type)
+                {
+                    case "Tragedy":
+                        thisAmount = 40000;
+                        if (perf.Audience > 30)
+                        {
+                            thisAmount += 1000 * (perf.Audience - 30);
+                        }
+                        break;
+
+                    case "Comedy":
+                        thisAmount = 30000;
+                        if (perf.Audience > 20)
+                        {
+                            thisAmount += 10000 + 500 * (perf.Audience - 20);
+                        }
+                        thisAmount += 3000 * perf.Audience;
+                        break;
+
+                    default:
+                        throw new Exception($"Unknown type:{play.PlayDetails.Type}");
+                }
+
+                volumeCredits += Math.Max(perf.Audience - 30, 0);
+
+                if ("Comedy" == play.PlayDetails.Type)
+                    volumeCredits += perf.Audience / 5;
+
+                result += $"{play.PlayDetails.Name}: {thisAmount / 100} ({perf.Audience} seats)";
+                totalAmount += thisAmount;
+            }
+
+            result += $"Amount owed is {totalAmount / 100}";
+            result += $"You earned {volumeCredits} credits";
+
+            Result = result;
+        }
+
+        private void CreateData()
+        {
+            Invoices = new List<Invoice>();
+            Plays = new List<Play>();
+
+            var performances = new List<Performance>();
+            performances.Add(new Performance()
+            {
+                PlayID = "hamlet",
+                Audience = 55
+            });
+            performances.Add(new Performance()
+            {
+                PlayID = "as-like",
+                Audience = 35
+            });
+            performances.Add(new Performance()
+            {
+                PlayID = "othelo",
+                Audience = 10
+            });
+
             Invoices.Add(new Invoice()
             {
                 Custumer = "BigCO",
-                Performances = new List<Performance>()
-                {
-                    new Performance()
-                    {
-                        PlayID = "hamlet",
-                        Audience = 55
-                    },
-                    new Performance()
-                    {
-                        PlayID = "as-like",
-                        Audience = 35
-                    },
-                    new Performance()
-                    {
-                        PlayID = "othelo",
-                        Audience = 10
-                    },
-                }
+                Performances = performances
             });
 
             Plays.Add(new Play()
@@ -80,13 +136,11 @@ namespace Refactoring.Cases.Case1
         public string Name { get; set; }
         public string Type { get; set; }
     }
-
     public class Invoice
     {
         public string Custumer { get; set; }
         public List<Performance> Performances { get; set; } = new List<Performance>();
     }
-
     public class Performance
     {
         public string PlayID { get; set; }
--- Refactoring.Cases/Program.cs
+++ Refactoring.Cases/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Refactoring.Cases.Case1;
+﻿using Refactoring.Cases.Case1;
 
-Console.WriteLine("Hello, World!");
-
-var teste = new Statement();
+var statement = new Statement();
+Console.WriteLine(statement.Result);
